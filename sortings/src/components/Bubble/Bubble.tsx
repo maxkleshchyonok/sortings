dfe2bc4b28205195059eea6bf58bf67cac674620
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2023-11-13 18:29:34 +0300

    fix: remove old comments

--- sortings/src/components/Bubble/Bubble.tsx
+++ sortings/src/components/Bubble/Bubble.tsx
@@ -2,52 +2,6 @@
 
 const Bubble = ({ array }: { array: number[] }) => {
 
-  // let time;
-
-  // const sort = () => {
-  //   const start = Date.now();
-  //   for (let i = 0; i < array.length; i++) {
-  //     for (let j = 0; j < array.length; j++) {
-  //       if (array[j] > array[j + 1]) {
-  //         let temp = array[j];
-  //         array[j] = array[j + 1];
-  //         array[j + 1] = temp;
-  //       }
-  //     }
-  //   }
-  //   const end = Date.now();
-  //   console.log(array);
-  //   time = end - start;
-  //   document.getElementById('time')!.textContent = time.toString() + ' ms';
-  // };
-
-  // async function bubbleSort() {
-  //   const start = Date.now();
-  //   for (let i = array.length; i > 0; i--) {
-  //     for (let j = 0; j < i - 1; j++) {
-  //       if (array[j] > array[j + 1]) {
-  //         await swap(array, j, j + 1);
-  //       }
-  //     }
-  //   }
-  //   const end = Date.now();
-  //   document.getElementById('time')!.textContent = `${end - start} ms`;
-  //   return array;
-  // }
-
-  // function swap(arr: number[], i: number, j: number) {
-  //   return new Promise<void>((res) => {
-  //     let temporary = arr[i];
-  //     arr[i] = arr[j];
-  //     arr[j] = temporary
-  //     console.log(arr);
-  //     setTimeout(() => {
-  //       res()
-  //     }, 0)
-  //   })
-  // }
-
-
   const bubbleSortWorker = new Worker('worker.js');
 
   const sort = () => {
@@ -58,9 +12,6 @@ const Bubble = ({ array }: { array: number[] }) => {
       document.getElementById('time')!.textContent = `${message.data} ms`;
     }
   };
-
-
-
 
   return (
     <div className="sortcontainer">
